ad198015e7216ec60e8f0c63e6922409bf6a8036
csivakolundu <csivakolundu@paypal.com>
2018-11-08 21:53:11 -0800

    working code.

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,14 @@ import 'package:flutter/material.dart';
 main() {
 runApp(MyApp());
 }
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+    State<StatefulWidget> createState() {
+      // TODO: implement createState
+      return _myState(); 
+    }
+}
+class _myState extends State<MyApp> {
   @override
     Widget build(BuildContext context) {
       
@@ -18,7 +25,7 @@ class MyApp extends StatelessWidget {
                   child: RaisedButton(                  
                   onPressed: (){
                   },
-                  child: Text('Click Me'),
+                  child: Text('Click Me!'),
               ))
                ,
               Card(child: Column(children: <Widget>[
